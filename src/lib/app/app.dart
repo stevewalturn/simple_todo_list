97dcc93a9bf8e934c320ffa29a3eb89697535915
stevewalturn <steve@walturn.com>
2024-12-15 22:30:30 +0800

    Update files (batch 1/1)

---
+++ src/lib/app/app.dart
@@ -0,0 +1,30 @@
+import 'package:simple_todo_list/ui/bottom_sheets/notice/notice_sheet.dart';
+import 'package:simple_todo_list/ui/dialogs/info_alert/info_alert_dialog.dart';
+import 'package:simple_todo_list/features/home/home_view.dart';
+import 'package:simple_todo_list/features/startup/startup_view.dart';
+import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
+// @stacked-import
+
+@StackedApp(
+  routes: [
+    MaterialRoute(page: HomeView),
+    MaterialRoute(page: StartupView),
+// @stacked-route
+  ],
+  dependencies: [
+    LazySingleton(classType: BottomSheetService),
+    LazySingleton(classType: DialogService),
+    LazySingleton(classType: NavigationService),
+    // @stacked-service
+  ],
+  bottomsheets: [
+    StackedBottomsheet(classType: NoticeSheet),
+    // @stacked-bottom-sheet
+  ],
+  dialogs: [
+    StackedDialog(classType: InfoAlertDialog),
+    // @stacked-dialog
+  ],
+)
+class App {}
